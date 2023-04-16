03c4cb88c7349a92fba7a566c06080f26256983d
Md. Arman Hossain <mahpro110@gmail.com>
2023-04-16 11:12:12 +0600

    data model schema as named Package create done

---
+++ models/Package.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const packageSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter a Package name"],
+      trim: true,
+      unique: [true, "Please Give a Unique Name"],
+      minLength: [3, "Name must be at least 3 character"],
+      maxLength: [100, "Name is too large"],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: [true, "Please Confirm You enter Duration"],
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price Must be a positive Number"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["hour", "day", "week"],
+        message: "unit value Can't be {Value}, should be hour/day/week",
+      },
+    },
+    image_url: {
+      type: String,
+      required: true,
+    },
+    view_count: {
+      type: Number,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+const Package = mongoose.model("Package", packageSchema);
+
+module.exports = Package;
